df57b09e106594b0815dfb1932523014aaab16a9
Jamie Webster <jamie_maxwell_webster@hotmail.com>
2016-02-08 18:57:52 -0500

    Changed DateTime to bool to match %t which  [GO] fmt.Fprintf says is a bool. This may require more testing. 0/1 vs. true/false

--- src/MatchMessages/Heal.cs
+++ src/MatchMessages/Heal.cs
@@ -6,20 +6,21 @@ using System.Threading.Tasks;
 /*
 {\"type\":25,\"time\":\"%s\",\"iat\":%t,\"iah\":%t,\"iti\":%t,\"ith\":%t,\"ivr\":%t,\"ivd\":%t,\"itb\":%t,\"attacker\":\"%s\",
 \"target\":\"%s\",\"target_source\":\"%s\",\"damage_source\":\"%s\",\"inflictor\":\"%s\",\"value\":%d},"
+
 */
 namespace HGV.Perserverance.MatchMessages
 {
     public class Heal : GameMessage
     {
 
-        public DateTime iat { get; set; }
-        public DateTime iah { get; set; }
-        public DateTime iti { get; set; }
-        public DateTime ith { get; set; }
-        public DateTime ivr { get; set; }
-        public DateTime ivd { get; set; }
-        public DateTime itb { get; set; }
-        public string attacker { get; set; }
+        public bool iat { get; set; } // GetIsAttackerIllusion
+		public bool iah { get; set; } // GetIsAttackerHero
+		public bool iti { get; set; } // GetIsTargetIllusion
+		public bool ith { get; set; } // GetIsTargetHero
+		public bool ivr { get; set; } // GetIsVisibleRadiant
+		public bool ivd { get; set; } // GetIsVisibleDire
+		public bool itb { get; set; } // GetIsTargetBuilding
+		public string attacker { get; set; }
         public string target { get; set; }
         public string targetSource { get; set; }
         public string damageSource { get; set; }
